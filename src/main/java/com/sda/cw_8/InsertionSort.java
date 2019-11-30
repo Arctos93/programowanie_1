a2792a93c40f1dad593f637150296976084c7d7d
k <krzysztof.dymek@gmail.com>
2019-11-30 12:10:45 +0100

    sorting - cd

--- src/main/java/com/sda/cw_8/InsertionSort.java
+++ src/main/java/com/sda/cw_8/InsertionSort.java
@@ -1,8 +1,28 @@
 package com.sda.cw_8;
 
+import java.util.Arrays;
+
 public class InsertionSort implements Sorter {
     @Override
     public void sort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int toInsert = arr[i];
+            int insertionIdx = findInsertionIndex(arr, i, toInsert);
+            moveForward(arr, insertionIdx, i);
+            arr[insertionIdx] = toInsert;
+        }
+    }
 
+    private void moveForward(int[] arr, int from, int to) {
+        System.arraycopy(arr, from, arr, from + 1, to - from);
+    }
+
+    private int findInsertionIndex(int[] arr, int currentIdx, int toInsert) {
+        for (int i = 0; i < currentIdx; i++) {
+            if(arr[i] > toInsert) {
+                return i;
+            }
+        }
+        return currentIdx;
     }
 }
