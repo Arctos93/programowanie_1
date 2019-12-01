4315851ed2f6c237bd95ec9c3339be58af7f1ec5
k <krzysztof.dymek@gmail.com>
2019-12-01 10:51:42 +0100

    bst - cd

--- src/main/java/com/sda/cw_13/BinarySearchTreeImpl.java
+++ src/main/java/com/sda/cw_13/BinarySearchTreeImpl.java
@@ -1,15 +1,89 @@
 package com.sda.cw_13;
 
 public class BinarySearchTreeImpl implements BinarySearchTree {
+
+    private Node root;
+
     @Override
     public void add(int element) {
+        if (isEmpty()) {
+            addToEmpty(element);
+        } else {
+            addToNotEmpty(element);
+        }
+    }
 
+    private void addToNotEmpty(int element) {
+        Node parent = findParentFor(element);
+        addNodeToParent(parent, element);
+    }
 
+    private Node findParentFor(int element) {
+        Node current = root;
+
+        while (element <= current.getValue() && current.hasLeft() ||
+                element > current.getValue() && current.hasRight()) {
+            if (element <= current.getValue()) {
+                current = current.getLeft();
+            } else {
+                current = current.getRight();
+            }
+        }
+
+        return current;
+    }
+
+    private void addNodeToParent(Node parent, int element) {
+        Node node = new Node(parent, element);
+        if (element <= parent.getValue()) {
+            parent.setLeft(node);
+        } else {
+            parent.setRight(node);
+        }
+    }
+
+    private void addToEmpty(int element) {
+        root = new Node(null, element);
     }
 
     @Override
     public boolean contains(int element) {
-        return false;
+       return contains_r(element);
+    }
+
+    private boolean contains_r(int element) {
+        return nodeContains(root, element);
+    }
+
+    private boolean nodeContains(Node node, int element) {
+        if (node == null) {
+            return false;
+        } else if (node.getValue() == element) {
+            return true;
+        } else if (element < node.getValue()) {
+            return nodeContains(node.getLeft(), element);
+        } else {
+            return nodeContains(node.getRight(), element);
+        }
+    }
+
+    private boolean contains_i(int element) {
+        if(isEmpty()) {
+            return false;
+        } else {
+            Node current = root;
+            while (true) {
+                if(element == current.getValue()) {
+                    return true;
+                } else if (element < current.getValue() && current.hasLeft()) {
+                    current = current.getLeft();
+                } else if (element > current.getValue() && current.hasRight()) {
+                    current = current.getRight();
+                } else {
+                    return false;
+                }
+            }
+        }
     }
 
     @Override
@@ -17,7 +91,9 @@ public class BinarySearchTreeImpl implements BinarySearchTree {
         return 0;
     }
 
-
+    private boolean isEmpty() {
+        return root == null;
+    }
 
     private class Node {
         private Node parent;
